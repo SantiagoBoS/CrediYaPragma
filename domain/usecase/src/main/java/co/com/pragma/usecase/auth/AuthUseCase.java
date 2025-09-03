dfb1b03219559d2b635c7df31993f116b971207f
SantiagoBoS <sborrerosuarez@gmail.com>
2025-09-03 16:17:44 -0500

    Feat: Ajuste de los logs en useCase

--- domain/usecase/src/main/java/co/com/pragma/usecase/auth/AuthUseCase.java
+++ domain/usecase/src/main/java/co/com/pragma/usecase/auth/AuthUseCase.java
@@ -4,31 +4,22 @@ import co.com.pragma.model.auth.exception.BusinessException;
 import co.com.pragma.model.auth.gateways.AuthRepository;
 import co.com.pragma.model.auth.gateways.TokenProvider;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
-@Slf4j
 @RequiredArgsConstructor
 public class AuthUseCase {
     private final AuthRepository authRepository;
     private final TokenProvider tokenProvider;
 
     public Mono<String> login(String email, String password) {
-        log.info("AuthUseCase - Iniciando autenticación para email{}", email);
-        return authRepository.findByEmail(email).switchIfEmpty(Mono.defer(() -> {
-                    log.warn("AuthUseCase - Usuario no encontrado con email{}", email);
-                    return Mono.error(new BusinessException("Usuario no encontrado"));
-                }))
+        return authRepository.findByEmail(email).switchIfEmpty(
+                Mono.error(new BusinessException("Usuario no encontrado")))
                 .flatMap(user -> {
-                    log.debug("AuthUseCase - Usuario encontrado con email={}", email);
                     if (!user.getPassword().equals(password)) {
-                        log.warn("AuthUseCase - Credenciales inválidas para email={}", email);
                         return Mono.error(new BusinessException("Credenciales inválidas"));
                     }
-                    log.info("AuthUseCase - Credenciales correctas, generando token para email={}", email);
                     return Mono.just(tokenProvider.generateToken(user));
                 })
-                .doOnSuccess(token -> log.debug("AuthUseCase - Token generado correctamente para email={}", email))
-                .doOnError(e -> log.error("AuthUseCase - Error en login para email={}: {}", email, e.getMessage(), e));
+                .onErrorResume(e -> Mono.error(new BusinessException("Error durante la autenticación: " + e.getMessage())));
     }
 }
